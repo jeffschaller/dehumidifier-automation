15ba0611461b5ea522027672163ae047c6bb5a82
Jeff Schaller <17769792+jeffschaller@users.noreply.github.com>
2023-11-04 17:09:47 -0400

    Update dehumidifier.ino
    added an arbitrary pin definition for talking to the relay & code to set the pin high or low; added debug for printing things

--- dehumidifier.ino
+++ dehumidifier.ino
@@ -21,34 +21,62 @@
 #include "Adafruit_APDS9960.h"
 Adafruit_APDS9960 apds;
 
+#define PIN_TO_RELAY 6
+
 void setup() {
   Serial.begin(115200);
 
-  if(!apds.begin()){
+  if(!apds.begin()) {
     Serial.println("failed to initialize device! Please check your wiring.");
   }
-  else Serial.println("Device initialized!");
+  else {
+    // Assume success
+    // Serial.println("Device initialized!");
+  }
 
-  //enable color sensing mode
   apds.enableColor(true);
+  pinMode(PIN_TO_RELAY, OUTPUT);
+  // start with the relay "open"
+  digitalWrite(PIN_TO_RELAY, LOW);
 }
 
 void loop() {
-  //create some variables to store the color data in; unused except for 'red'
+  // create some variables to store the color data in; mostly unused, except for 'red'
   uint16_t red, g, b, c;
+  int relay_pin_state = 0;
+  int debug = 0;
   
-  //wait for color data to be ready
+  // wait for color data to be ready
   while(!apds.colorDataReady()){
     delay(5);
   }
-
-  //get fresh data
+  // get fresh data
   apds.getColorData(&red, &g, &b, &c);
   Serial.print("red value is:"); Serial.println(red);
+
+  if (debug) {
+    relay_pin_state = digitalRead(PIN_TO_RELAY);
+    if (relay_pin_state == HIGH) {
+      Serial.println("steadystate: Relay pin is HIGH");
+    } else {
+      Serial.println("steadystate: Relay pin is LOW");
+    }
+  }
+
   if(red > 50) {
     Serial.println("start the pump (for 5 sec)");
+    digitalWrite(PIN_TO_RELAY, HIGH);
+    if (debug) {
+      relay_pin_state = digitalRead(PIN_TO_RELAY);
+      if (relay_pin_state == HIGH) {
+        Serial.println("pumping: Relay pin is HIGH");
+      } else {
+        Serial.println("pumping: Relay pin is LOW");
+      }
+    }
     delay(5 * 1000);  /* 5 sec in ms; TODO: eventually 4min */
     Serial.println("stop the pump");
+    digitalWrite(PIN_TO_RELAY, LOW);
   }
-  delay(1 * 1000);  /* 30 sec in ms */
+  delay(1 * 1000);  /* 1 sec in ms; TODO: increase when ready */
 }
